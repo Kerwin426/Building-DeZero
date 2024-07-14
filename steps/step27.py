377e0a629d19e4a6a86a3202c97c07b348dda955
Kerwin426 <2901997993@qq.com>
2024-07-15 00:46:36 +0800

    做到步骤28了

--- steps/step27.py
+++ steps/step27.py
@@ -1,25 +1,49 @@
+
 if '__file__' in globals():
     import os
     import sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    
+import math
+from dezero import Variable
 import numpy as np
 from dezero import Function
-
+from dezero.utils import plot_dot_graph 
 class Sin(Function):
     def forward(self, x):
         y = np.sin(x)
         return y
-    def backward(self,gy):
+
+    def backward(self, gy):
         x = self.inputs[0].data
         gx = gy*np.cos(x)
         return gx
+
+
 def sin(x):
     return Sin()(x)
 
-from dezero import Variable
+
 x = Variable(np.array(np.pi/4))
 y = sin(x)
 y.backward()
 print(y.data)
 print(x.grad)
+
+
+def my_sin(x, threshold=1e-150):
+    y = 0
+    for i in range(100000):
+        c = (-1) ** i / math.factorial(2*i+1)
+        t = c*x ** (2*i + 1)
+        y = y+t
+        if abs(t.data) < threshold:
+            break
+    return y
+
+x = Variable(np.array(np.pi/4))
+y = my_sin(x)
+y.backward()
+print(y.data)
+print(x.grad)
+
+plot_dot_graph(y ,verbose=False,to_file='my_sin_1e-150.png')
